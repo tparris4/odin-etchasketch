bbc5ebc48d0396b206e32a2eabb41e3f895d32ab
Tyler Parris <tymania421@gmail.com>
2022-03-02 00:50:25 -0500

    added button events

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+function gridCreate(row, col){
+    container.style.setProperty('.divsq', row);
+    container.style.setProperty('.divsq', col);
+    for(let i = 0; i < row; i++){
+        squareCreate(i);
+    }
+    for(let i = 0; i < col; i++){
+        squareCreate(i);
+    }
+}
+
 function squareCreate(input){
     for(let i = 0; i < input; i++){
             const divsq = document.createElement('div');
             divsq.classList.add('divsq');
             container.appendChild(divsq);
-        
+            divsq.addEventListener('mouseover', (e) => {
+                e.target.style.color = "black";
+            })
     }
+    
 }
 
 function removeSq(){
@@ -14,25 +28,21 @@ function removeSq(){
     }
 }
 
-const container = document.querySelector('.container');
-const btnReset = document.querySelector('reset');
-
-
-
-divsq.addEventListner("mouseenter", (e) => {
-    e.target.style.color = "blue";
-})
-
-
 btnReset.addEventListener("click", () => {
 
     let input = prompt("How many squares do you want? Max: 100 squares, Min: 4");
-    if(input < 100 && input > 3){
-    squareCreate(input);
-    }
-    else{
-        squareCreate(100);
+    if(input < 64 && input > 16){
+    gridCreate(input, input);
     }
 })
 
-squareCreate(9);
+// document.querySelectorAll('.divsq').forEach(divsq => {
+//     divsq.addEventListener('mouseover', (e) => {
+//         e.target.style.color = "white";
+//     });
+// });
+
+const container = document.querySelector('#container');
+const btnReset = document.querySelector('reset');
+
+gridCreate(16, 16);
