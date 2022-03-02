3da6ae96bb96f0620e4658d4a46e3d027acef20f
Tyler Parris <tymania421@gmail.com>
2022-03-02 02:19:35 -0500

    grid

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 function gridCreate(row, col){
-    container.style.setProperty('.divsq', row);
-    container.style.setProperty('.divsq', col);
     for(let i = 0; i < row; i++){
         squareCreate(i);
     }
@@ -14,35 +12,34 @@ function squareCreate(input){
             const divsq = document.createElement('div');
             divsq.classList.add('divsq');
             container.appendChild(divsq);
-            divsq.addEventListener('mouseover', (e) => {
-                e.target.style.color = "black";
-            })
-    }
+            container.style.setProperty('.divsq', divsq);
+            divsq.addEventListener("mouseover", (e) => {
+                e.target.style.backgroundColor = "black";
+            });
+    };
     
 }
 
 function removeSq(){
-    const sq = document.querySelectorAll('div.divsq');
+    const sq = document.querySelectorAll('divsq');
     for (const elem of sq){
         elem.remove();
     }
 }
 
-btnReset.addEventListener("click", () => {
 
-    let input = prompt("How many squares do you want? Max: 100 squares, Min: 4");
+
+
+const container = document.querySelector('.container');
+const btnReset = document.querySelector('.reset');
+
+gridCreate(16, 16);
+
+btnReset.addEventListener("click", () => {
+    removeSq();
+    let input = prompt("How many squares do you want? Max: 64 squares, Min: 16");
     if(input < 64 && input > 16){
     gridCreate(input, input);
     }
 })
 
-// document.querySelectorAll('.divsq').forEach(divsq => {
-//     divsq.addEventListener('mouseover', (e) => {
-//         e.target.style.color = "white";
-//     });
-// });
-
-const container = document.querySelector('#container');
-const btnReset = document.querySelector('reset');
-
-gridCreate(16, 16);
